d0fa21908efb562ce9407a00921a466d1c443538
Rajan Bhadauria <rajanbhadauria@gmail.com>
2025-09-12 11:36:42 +0530

    Online checkin link is in progress

--- pages/bookingListingPage.js
+++ pages/bookingListingPage.js
@@ -2,20 +2,37 @@ import { expect } from '@playwright/test';
 
 class BookingListingPage {  
   constructor(page){
-    this.page = page;
+    this.page = page
+    this.cancelBtn = page.locator('#add_booking_close_modal');
     this.addBookingBtn = page.locator('#add_booking_button');
+
+    this.propertyListingPlaceholder = page.locator('#assigned_property');
+    this.propertyItems = page.locator('span#assigned_property .multiselect_input_container');
     this.propertyListing = page.locator('#assigned_property li.list_item');
+
+    this.rentalListingPlaceholder = page.locator('#assigned_rental');
+    this.rentalItems = page.locator('span#assigned_rental .multiselect_input_container');
     this.rentalUnitListing = page.locator('#assigned_rental li.list_item');
+
+    this.datePlaceholder = page.locator("div.custom-date-box");
+
+    this.bookingSourcePlaceholder = page.locator("#booking_source .multiselect_input_container")
     this.bookingSourceList = page.locator('//span[@id="booking_source"]//li//label');
     this.bookingStatusSelect = page.locator('#reservation_status');
+
     this.bookingPriceInput = page.locator('#total_booking_amount');
     this.bookingInternalNotes = page.locator('#bookingNotes');
     this.firstNameInput = page.locator('#first_name');
     this.lastNameInput = page.locator('#last_name');
     this.emailInput = page.locator('#email');
     this.phoneInput = page.locator('.iti__tel-input[data-intl-tel-input-id="3"]').first();
-    this.submitButton = page.locator('div.mt-3 button[name="Save Changes"]');
-    this.bookings = [];
+    this.submitButton =  page.getByRole('button', { name: 'Save Changes' });
+    this.loadingSpinner = page.locator('div.custom-loading-container');
+
+    this.bookingActionsBtn = page.locator('.booking-list-card-item #moreMenu');
+    this.expandedActions = page.locator("//div[@class='booking-list-card-item']//span[@class='translate d-flex justify-content-between w-100 guest-link-item'][normalize-space()='Guest Link']");
+    this.shareLinkBtn = page.locator("//div[contains(@class,'dropdown-menu dropdown-menu-right notranslate booking-list-card-item-more-menu-dropdown show')]//ul[contains(@class,'preferences_child')]//span[@data-label='Share' and normalize-space()='Share']");
+    this.shareLinkInput = page.locator('#shareBookingModal_linkCopyInput');
   }
 
   getFutureDate(daysAfterToday = 10) {
@@ -34,9 +51,28 @@ class BookingListingPage {
     return locator.nth(randomIndex);
   }
 
+  async copyPrecheckinLink() {
+    console.log('Copying pre-checkin link');
+    await this.loadingSpinner.first().waitFor({ state: 'hidden', timeout: 10000 });
+    await expect(this.bookingActionsBtn).toBeVisible();
+    this.bookingActionsBtn.click();
+    await this.expandedActions.first().waitFor({ state: 'visible', timeout: 5000 });
+    await expect(this.expandedActions.first()).toBeVisible();
+    await this.expandedActions.first().click();
+    await this.shareLinkBtn.first().waitFor({ state: 'visible', timeout: 5000 });
+    await expect(this.shareLinkBtn.first()).toBeVisible();
+    await this.shareLinkBtn.first().click();
+    await this.shareLinkInput.waitFor({ state: 'visible', timeout: 5000 });
+    await expect(this.shareLinkInput).toBeVisible();
+    const precheckinLink = await this.shareLinkInput.inputValue();
+    console.log('Pre-checkin link copied:', precheckinLink);
+    return precheckinLink;
+  }
+
   async navigate() {
     console.log('Navigating to booking listing page');
     await this.page.goto('/client/v2/bookings', { waitUntil: 'load' });
+    
   }
 
   async addBooking(booking) {
@@ -44,38 +80,60 @@ class BookingListingPage {
     const { faker } = await import('@faker-js/faker');
 
     console.log('Adding a new booking');
+    await this.loadingSpinner.first().waitFor({ state: 'hidden', timeout: 10000 });
+    await expect(this.addBookingBtn).toBeVisible();
     await this.addBookingBtn.click();
+    
+    console.log('Waiting to hide loading spinner');
+    await this.loadingSpinner.first().waitFor({ state: 'hidden', timeout: 10000 });
+    await this.cancelBtn.waitFor({ state: 'visible', timeout: 5000 });
+    await expect(this.cancelBtn).toBeVisible();
 
+    console.log('Selecting property unit');
+    await this.propertyListingPlaceholder.waitFor({ state: 'visible' });
+    await this.page.waitForTimeout(2000); // Wait for the property list to populate
+    this.propertyItems.click()
+    //await expect(this.propertyItems).first().toBeVisible();
     const property = await this.selectRandomItem(this.propertyListing);
     await property.click();
 
-    const rentalUnit = await this.selectRandomItem(this.rentalUnitListing);
-    await rentalUnit.click();
+    console.log('Selecting rental unit');
+    await this.rentalListingPlaceholder.waitFor({ state: 'visible' });
+    await this.rentalItems.click()
+    if (await this.rentalUnitListing.count() >1) {
+      const rentalUnit = await this.selectRandomItem(this.rentalUnitListing);
+      await rentalUnit.click();
+    } else {
+      console.log('Only one rental unit available, selecting it');
+    }
 
+    console.log('Selecting check-in and check-out dates');
+    await this.datePlaceholder.first().click();
+    // Select check-in date 10 days from today and check-out date 20 days from today
     const checkinDateSelector = `//div[@id='${this.getFutureDate(10)}']//div[contains(@class, 'dp__pointer')]`;
     const checkOutDateSelector = `//div[@id='${this.getFutureDate(20)}']//div[contains(@class, 'dp__pointer')]`;
     await this.page.locator(checkinDateSelector).click();
     await this.page.locator(checkOutDateSelector).click();
 
+    console.log('Selecting booking source and status');
+    await this.bookingSourcePlaceholder.click();
     const bookingSource = await this.selectRandomItem(this.bookingSourceList);
     await bookingSource.click();
-
-    await this.bookingStatusSelect.selectOption(booking.status);
+    
+    //await this.bookingStatusSelect.selectOption(booking.status);
     await this.bookingPriceInput.fill((Math.random() * (1500 - 1000) + 1000).toFixed(2));
     await this.bookingInternalNotes.fill(faker.lorem.sentence());
     await this.firstNameInput.fill(faker.person.firstName());
     await this.lastNameInput.fill(faker.person.lastName());
     await this.emailInput.fill(faker.internet.email());
-    await this.phoneInput.fill(faker.phone.number());
+    await this.phoneInput.fill("+919451011111");//faker.phone.number({ style: 'human' }));
     await this.submitButton.click();
 
-    this.bookings.push(booking);
-    await this.page.waitForTimeout(10000);
+    console.log('Waiting for booking to be added and loading spinner to disappear');
+    await this.loadingSpinner.first().waitFor({ state: 'hidden', timeout: 20000 });
+    console.log('Booking added successfully');
   }
 
-  removeBooking(bookingId) {
-    this.bookings = this.bookings.filter(b => b.id !== bookingId);
-  }
 
   getBookings() {
     return this.bookings;
--- tests/booking.spec.js
+++ tests/booking.spec.js
@@ -3,7 +3,7 @@ import LoginPage from '../pages/loginPage';
 import { BookingListingPage } from '../pages/bookingListingPage';
 const loginData = require('../utils/variables.json');
 
-test('add a new booking', async ({ page }) => {
+test.skip('add a new booking', async ({ page }) => {
     const login = new LoginPage(page);
     await login.completeLoginProcess(
         loginData.login_master.email,
@@ -15,4 +15,25 @@ test('add a new booking', async ({ page }) => {
     const bookingListing = new BookingListingPage(page);
     await bookingListing.navigate();
     await bookingListing.addBooking({ status: 'confirmed' }); // Pass booking object if needed
+});
+
+test('pre-checkin link copy', async ({ page }) => {
+    const login = new LoginPage(page);
+    await login.completeLoginProcess(
+        loginData.login_master.email,
+        loginData.login_master.password,
+        loginData.login_master.otp
+    );
+    console.log('Login successful');
+    await login.assertRedirectedToDashboard();
+
+    const bookingListing = new BookingListingPage(page);
+    console.log('Navigating to booking listing page');
+    await bookingListing.navigate();
+
+    // const copiedLink = await bookingListing.copyPrecheckinLink();
+    // console.log('Copied Pre-checkin Link:', copiedLink);
+    // const newContext = await browser.newContext();
+    // const newPage = await newContext.newPage();
+    // await newPage.goto(copiedLink);
 });
